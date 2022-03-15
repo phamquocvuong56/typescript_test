8836c4836247dbb2ea35a15ed50455e607f97971
Vuong <phamquocvuong56k2@gmail.com>
2022-03-15 11:14:50 +0700

    create prj

---
+++ src/components/Test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Test() {
+    var message: string = "Hello World"
+    console.log(message)
+    return (
+        <div>
+            {message}
+        </div>
+    )
+}
